d47a0941fb11ebb50c5a89e580d7b97cbc1d21aa
Adegboro Moradeyo <iremamae2018@gmail.com>
2024-05-10 15:01:45 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status"""
+"""
+A Python script that fetches https://alx-intranet.hbtn.io/status
+"""
 
 import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-
+    url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
-        content = response.read()
-
+        body = response.read()
         print("Body response:")
-        print("    - type:", type(content))
-        print("    - content:", content)
-        print("    - utf8 content:", content.decode('utf-8'))
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode('utf-8'))
