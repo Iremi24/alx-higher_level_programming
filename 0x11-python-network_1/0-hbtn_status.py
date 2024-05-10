70a7258b69c7bd620448070b6bc59a7f4bf2a3c9
Adegboro Moradeyo <iremamae2018@gmail.com>
2024-05-10 14:50:22 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+
+        print("Body response:")
+        print("    - type:", type(content))
+        print("    - content:", content)
+        print("    - utf8 content:", content.decode('utf-8'))
